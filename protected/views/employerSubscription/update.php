061e3c03855b66e4ca6da8f2928559a95abd966a
lawrence m <lawrence.manickam@gmail.com>
2013-08-18 18:48:39 -0400

    Revert "sample"
    This reverts commit 561cd0fa0cf8b420222a2c08ab7fe5d89ec00868.

--- protected/views/employerSubscription/update.php
+++ protected/views/employerSubscription/update.php
@@ -18,6 +18,4 @@ $this->menu=array(
 <!--
 <h1>Update EmployerSubscription <?php //echo $model->ESUB_ID; ?></h1>-->
 
-Thisis a test.
-
 <?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
