0e896da85b2640c5de9140f2da0071bb02c225d3
lawrence m <lawrence.manickam@gmail.com>
2013-08-18 18:55:03 -0400

    yo

--- protected/views/employerSubscription/order1.php
+++ protected/views/employerSubscription/order1.php
@@ -1,44 +1,44 @@
 
-<?php 
+<?php
 
 $deleteUrl1 =  Yii::app()->createAbsoluteUrl('Paypal/order');
 $upsub =  Yii::app()->createAbsoluteUrl('Site/Subscription',array('update'=>1));
 $clearcart =  Yii::app()->createAbsoluteUrl('EmployerSubscription/removesuball');
- 
+
 
 ?>
 <div class="Container">
-  <div style="margin:60px auto; width:99.4%" >
+  <div style="margin:60px auto; width:100.4%" >
 
-    	<div style="color:#F00"><?php //echo $msg?></div>
+    	<div style="color:#F001"><?php //echo $msg?></div>
     	<table border="0" cellpadding="5px" cellspacing="1px" style=" font-size:13px; background-color:#E1E1E1" width="100%">
             	<tr bgcolor="#FFFFFF" style="font-weight:bold">
                     <td>Serial</td>
-                    <td>Name</td> 
+                    <td>Name</td>
                     <td>Amount</td>
                     <td>Options</td>
                 </tr>
-			
-		<?php 
-             
-              
+
+		<?php
+
+
                   $total='';
                  $var= TempTransDetail::model()->findAllByAttributes(array('ttd_session_id'=>Yii::app()->session->sessionID));
                   $j=1;
 		  foreach ($var as $var1){
- 
+
 
         $data1= SubscriptionMaster::model()->findByAttributes(array('SUBSCRS_ID'=>$var1->ttd_sub_id));
         $data1->SUBSCR_DESC;
         $total +=$data1->SUBSCR_RATE; //$data1->SUBSCRS_ID;
-     
+
 		?>
             	<tr bgcolor="#FFFFFF">
                     <td><?php echo $j;?></td>
                     <td><?php  echo $data1->SUBSCR_DESC;?></td>
                     <td style="text-align: right">$<?php echo $data1->SUBSCR_RATE;?></td>
                     <td><a onclick="clear_cart('<?php echo $var1->ttd_td_id; ?>','<?php  echo $var1->ttd_id; ?>')" style="text-decoration:underline" >Remove</a></td></tr>
-            <?php					
+            <?php
 				$j++;}
 			?>
                 <tr><td colspan="5" align="right"><b>Order Total: $ <?php  echo $total;?></b></td></tr>
@@ -54,7 +54,7 @@ $clearcart =  Yii::app()->createAbsoluteUrl('EmployerSubscription/removesuball')
 //			}	echo "<tr bgC
 		?>
         </table>
-    </div>  
+    </div>
 </div>
 <script type="text/javascript">
 function clear_cart(ttd_td_id,key)
@@ -65,7 +65,7 @@ function clear_cart(ttd_td_id,key)
     url: '<?php echo Yii::app()->createAbsoluteUrl("EmployerSubscription/removesub"); ?>',
      data:"key="+key+"&ttd_td_id="+ttd_td_id+"&sid="+Math.random(),
     success:function(data){
-    
+
         if(data==0)
      {
          window.location.href='<?php echo $upsub;?>';
@@ -75,12 +75,12 @@ function clear_cart(ttd_td_id,key)
         window.location.reload();
      }
  }
- });  
+ });
 
   //return true;
 
 }
-    
+
 </script>
 
 <style>
